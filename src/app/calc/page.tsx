e5ee27656681b50ec4bb1b08234198df7dc307fe
Vidhan Raval <214603023+Vidhanraval@users.noreply.github.com>
2025-06-11 17:20:27 +0530

    changed

--- src/app/calc/page.tsx
+++ src/app/calc/page.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Calc from '@/components/Calc';
 
-export default function App() {
-  return (
-    <div className="min-h-screen bg-[#3a4764] flex items-center justify-center p-4">
-      <Calc />
-    </div>
-  );
+export default function Page() {
+  return <Calc />;
 }
+
