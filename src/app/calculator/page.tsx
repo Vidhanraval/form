af260708ea6bdae2778f5f31dcac51cd8e4134f7
Nidhi Prajapati <niddhichauhan7@gmail.com>
2025-06-11 17:55:26 +0530

    added

---
+++ src/app/calculator/page.tsx
@@ -0,0 +1,12 @@
+import Calculator from '@/components/Calculator/index';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  )
+}
+
+export default page
